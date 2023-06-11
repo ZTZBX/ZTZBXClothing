562be52d151017e8106bfa111205f7d87bfc3982
ZABBIX <zabbix@ztrunk.space>
2023-06-11 14:55:49 +0200

    Update load_me_in_database.sql

--- load_me_in_database.sql
+++ load_me_in_database.sql
@@ -13,13 +13,13 @@ CREATE TABLE itemidingame (
 
 /* Air Jordan 11 */
 INSERT INTO `itemsmetadata` (`name`, `image`, `descriptiontitle`, `description`, `type`, `unit`) VALUES (
- 'Air Jordan 11',
+ 'AirJordan11',
  'air_jordan_11.png',
  'Air Jordan 11 ',
  'Inspired by classic Jordan sneakers, these low-tops up your comfort with an extra-soft leather toe. Underfoot is soft, smooth foam with rubber pods in just the right spots for traction while webbing loops make lacing quick and easy.',
  'clothing-shoes', 'UNT'
 );
 
-INSERT INTO `itemidingame` (`item`, `id`) VALUES ('Air Jordan 11', '16');
-INSERT INTO `itemvariations` (`item`, `variationid`, `variationname`) VALUES ('Air Jordan 11', '0', 'black & red');
-INSERT INTO `itemvariations` (`item`, `variationid`, `variationname`) VALUES ('Air Jordan 11', '1', 'white');
+INSERT INTO `itemidingame` (`item`, `id`) VALUES ('AirJordan11', '16');
+INSERT INTO `itemvariations` (`item`, `variationid`, `variationname`) VALUES ('AirJordan11', '0', 'black & red');
+INSERT INTO `itemvariations` (`item`, `variationid`, `variationname`) VALUES ('AirJordan11', '1', 'white');
