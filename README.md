c9a272b8591b16648e09ccdb1fba7f409460d613
zabbix-byte <zabbix@ztrunk.space>
2023-06-11 12:43:33 +0200

    first item

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ZTZBXClothing
 This is a stream Clothing for ZTZBX
+
+
+### **Requirements**
+- [core-ztzbx](https://github.com/ZTZBX/core-ztzbx)
+- [inventory](https://github.com/ZTZBX/inventory)
+- [fivem-mysql](https://github.com/ZTZBX/fivem-mysql)
+
+To install add `load_me_in_database.sql` in the database;
+
+#### **Shoes**
+- ID: *17* 
+    - NAME: Air Jordan 11
+	    - Variant:
+            - 0: black & red
+	        - 1: white
---
+++ load_me_in_database.sql
@@ -0,0 +1,25 @@
+CREATE TABLE itemvariations (
+    item varchar(50) NOT NULL,
+    variationid int NOT NULL,
+    variationname varchar(255) NOT NULL,
+    FOREIGN KEY (item) REFERENCES itemsmetadata(`name`)
+)ENGINE=InnoDB;
+
+CREATE TABLE itemidingame (
+    item varchar(50) NOT NULL,
+    id int NOT NULL,
+    FOREIGN KEY (item) REFERENCES itemsmetadata(`name`)
+)ENGINE=InnoDB;
+
+/* Air Jordan 11 */
+INSERT INTO `itemsmetadata` (`name`, `image`, `descriptiontitle`, `description`, `type`, `unit`) VALUES (
+ 'Air Jordan 11',
+ 'air_jordan_11.png',
+ 'Air Jordan 11 ',
+ 'Inspired by classic Jordan sneakers, these low-tops up your comfort with an extra-soft leather toe. Underfoot is soft, smooth foam with rubber pods in just the right spots for traction while webbing loops make lacing quick and easy.',
+ 'clothing-shoes', 'UNT'
+);
+
+INSERT INTO `itemidingame` (`item`, `id`) VALUES ('Air Jordan 11', '17');
+INSERT INTO `itemvariations` (`item`, `variationid`, `variationname`) VALUES ('Air Jordan 11', '0', 'black & red');
+INSERT INTO `itemvariations` (`item`, `variationid`, `variationname`) VALUES ('Air Jordan 11', '1', 'white');
